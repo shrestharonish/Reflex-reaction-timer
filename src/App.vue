7fef0ebd9ec240dcd267086982a6b1f7793dadcf
Ronish Shrestha <ronish@gmail.com>
2022-10-11 18:47:21 +0545

    complete reflex reaction timer

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <h1>Reflex Timer</h1>
+  <button @click="start" :disabled="isPlaying === true">Play</button>
+  <Block v-if="isPlaying === true" :delay="delay" @end="endGame" />
+  <Results v-if="showResults" :score="score" />
+</template>
+
+<script>
+import Block from "./components/Block.vue";
+import Results from "./components/Results.vue";
+
+export default {
+  name: "App",
+  components: {
+    Block,
+    Results,
+  },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false,
+    };
+  },
+  methods: {
+    start() {
+      (this.delay = 2000 + Math.random() * 5000),
+        (this.isPlaying = true),
+        (this.showResults = false),
+        console.log(this.delay);
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime;
+      this.isPlaying = false;
+      this.showResults = true;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Results.vue
@@ -0,0 +1,47 @@
+<template>
+  <p>Reaction Time: {{ score }}</p>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+  props: ["score"],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = "Ninja Reflex";
+    } else if (this.score < 400) {
+      this.rank = "Rapid Reflex";
+    } else {
+      this.rank = "Turtle pace ....!";
+    }
+  },
+};
+</script>
+
+<style>
+.rank {
+  font-size: 1.6rem;
+  color: #0faf87;
+  font-weight: bold;
+}
+button {
+  background: #0faf87;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled] {
+  opacity: 0.2;
+  cursor: not-allowed;
+}
+</style>
